53745d0aba46dfa60e9f4e3586ffaacf7af6ba48
Alonso2880 <a80alonso@gmail.com>
2025-02-24 11:09:15 +0100

    añadido movimiento del jugador

--- Rancho_de_gorditos/Assets/mov.cs
+++ Rancho_de_gorditos/Assets/mov.cs
@@ -2,23 +2,24 @@ using UnityEngine;
 
 public class mov : MonoBehaviour
 {
-    private float vel;
-    private Rigidbody rb;
-    private float cordX, cordY, cordZ;
+    public float vel;
+    private float cordX, cordZ;
     private Vector3 pos;
 
     void Start()
     {
-        vel = 8.9f;
-        rb = GetComponent<Rigidbody>();
+        
+        
     }
 
-    void FixedUpdate()      {
-        cordY = rb.linearVelocity.y;  
+    void Update()
+    {
+         
         cordX = Input.GetAxis("Horizontal") * vel;
         cordZ = Input.GetAxis("Vertical") * vel;
+        pos = new Vector3(cordX, 0, cordZ);
 
-        pos = new Vector3(cordX, cordY, cordZ);
-        rb.linearVelocity = pos;  
+        transform.Translate(pos*vel*Time.deltaTime);
+          
     }
 }
