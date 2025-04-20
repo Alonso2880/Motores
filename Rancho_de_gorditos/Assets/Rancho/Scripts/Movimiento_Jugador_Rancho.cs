9a81b4961ca5d3117ecb8b4b01f914d35ae25e13
Alonso2880 <a80alonso@gmail.com>
2025-04-20 17:15:04 +0200

    brujita corriendo

--- Rancho_de_gorditos/Assets/Rancho/Scripts/Movimiento_Jugador_Rancho.cs
+++ Rancho_de_gorditos/Assets/Rancho/Scripts/Movimiento_Jugador_Rancho.cs
@@ -2,19 +2,21 @@ using UnityEngine;
 
 public class Movimiento_Jugador_Rancho : MonoBehaviour
 {
-    public float vel, velRot;
+    public float vel, velRot, velCorrer, velCopia;
+    public Animator animator;
     private float cordX, cordZ;
     private Vector3 pos; 
+    
     void Start()
     {
-
+        velCopia = vel;
     }
 
 
     void Update()
     {
 
-
+        
         cordX = Input.GetAxis("Horizontal");
         cordZ = Input.GetAxis("Vertical");
         pos = new Vector3(cordX, 0, cordZ).normalized;
@@ -27,6 +29,25 @@ public class Movimiento_Jugador_Rancho : MonoBehaviour
         }
 
         transform.Translate(pos * vel * Time.deltaTime, Space.World);
+        if(pos!= Vector3.zero)
+        {
+            animator.SetBool("Andar", true);
+        }
+        else
+        {
+            animator.SetBool("Andar", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            vel = velCorrer;
+            animator.SetBool("Correr", true);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            vel= velCopia;
+            animator.SetBool("Correr", false);
+        }
     }
 }
 
