580c4b4fb8cce0d8a0380568ac7a416a2bdaaa07
Victordm0 <victor05dm@gmail.com>
2025-04-22 12:11:54 +0200

    Comentarios

--- Rancho_de_gorditos/Assets/Ejercicios clase/Script/Ejercicio 6/CameraSwitcher.cs
+++ Rancho_de_gorditos/Assets/Ejercicios clase/Script/Ejercicio 6/CameraSwitcher.cs
@@ -3,36 +3,40 @@ using TMPro;
 
 public class CameraSwitcher : MonoBehaviour
 {
-    public Camera[] cameras;
+    public Camera[] cameras; //Array de cámaras
     public TextMeshProUGUI cameraLabelText;
-    private int currentCameraIndex = 0;
+    private int currentCameraIndex = 0; //Contador de cámaras del array
 
     void Start()
     {
-        ActivateCamera(currentCameraIndex);
+        ActivateCamera(currentCameraIndex);//Activa la cámara inicial
     }
 
-    public void SwitchToNextCamera()
+    public void SwitchToNextCamera() //Función para cambiar de cámara
     {
-        currentCameraIndex++;
-        if (currentCameraIndex >= cameras.Length)
+        currentCameraIndex++; //Suma 1 al contador
+        if (currentCameraIndex >= cameras.Length) 
+            //Si el numero del contador es mayor al numero de cámaras en el array
         {
-            currentCameraIndex = 0;
+            currentCameraIndex = 0; //Pone el contador en el inicio
         }
 
-        ActivateCamera(currentCameraIndex);
+        ActivateCamera(currentCameraIndex); 
+        //Llama a la función ActivateCamera
     }
 
-    void ActivateCamera(int index)
+    void ActivateCamera(int index) //Función para activar la cámara según
+        //el número del array
     {
-        for (int i = 0; i < cameras.Length; i++)
+        for (int i = 0; i < cameras.Length; i++) //Recorre el array
         {
-            cameras[i].gameObject.SetActive(i == index);
+            cameras[i].gameObject.SetActive(i == index); //Activa la cámara
         }
 
         if (cameraLabelText != null)
         {
-            cameraLabelText.text = cameras[index].name;
+            //Pone el nombre de la cámara en pantalla
+            cameraLabelText.text = cameras[index].name; 
         }
     }
 }
