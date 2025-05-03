b053b923126f089624d8fe1fc701de4f682aaaf1
Alonso2880 <a80alonso@gmail.com>
2025-05-03 12:26:33 +0200

    inventario fase 1

--- Rancho_de_gorditos/Assets/Rancho/Scripts/InventoryUI.cs
+++ Rancho_de_gorditos/Assets/Rancho/Scripts/InventoryUI.cs
@@ -1,24 +1,37 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Unity.VisualScripting;
+using System.Collections.Generic;
 
 public class InventoryUI : MonoBehaviour
 {
+    [HideInInspector] public List<InventoryItemData> inventario = new List<InventoryItemData>();
+
     public Transform itemsParent;
     public GameObject inventoryPanel;
     public GameObject itemUIPrefab;
     private Canvas c;
     public Button salir;
-    //[HideInInspector] public guardar_Inventario inventory;
-    
+    private bool adios=false;
 
+    private GameObject player;
+
+    public Image Icono1;
+    public Button Icono1Button;
+    private bool Icono1B = false;
+    public Sprite Spritehuevo;
+
+    public Image PolariodHuevo;
+    public Image TextoHuevo;
     
     void Start()
     {
-        //inventory = FindFirstObjectByType<guardar_Inventario>(FindObjectsInactive.Include); 
+        
         c = inventoryPanel.GetComponent<Canvas>();
         c.enabled = false;
         salir.onClick.AddListener(() => ads());
+        player = GameObject.Find("Player");
 
     }
 
@@ -32,13 +45,46 @@ public class InventoryUI : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.I))
         {
-            c.enabled = !c.enabled;
-            Time.timeScale = c.enabled ? 0 : 1;
-            if (c.enabled)
-                UpdateUI();
+            
+                c.enabled = !c.enabled;
+                Time.timeScale = c.enabled ? 0 : 1;
+                if (c.enabled)
+                {
+                    UpdateUI();
+                }
+                adios = true;
         }
+
+        MostrarIconos();
     }
 
+
+    private void MostrarIconos()
+    {
+        var inventory = guardar_Inventario.Instance;
+        InventoryItemData huevoItem = inventory.inventario.Find(item => item.nombre == "Huevo");
+
+        if(huevoItem != null && huevoItem.count >= 1)
+        {
+            Debug.Log("Huevo");
+
+            if (!Icono1B)
+            {
+                Icono1.sprite = Spritehuevo;
+                Icono1.enabled = true;
+                Icono1Button.onClick.AddListener(InfoHuevo);
+            }
+        }
+
+
+    }
+
+
+    private void InfoHuevo()
+    {
+        PolariodHuevo.enabled = true;
+        TextoHuevo.enabled = true;
+    }
     public void UpdateUI()
     {
         // Destruye todos los elementos viejos
