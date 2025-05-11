068fb75f2716bdc661b3e9f10642d5fb86e8401d
Alonso2880 <a80alonso@gmail.com>
2025-05-11 09:19:57 +0200

    .

--- Rancho_de_gorditos/Assets/Rancho/Scripts/Añadir_Mejorar_Parcela.cs
+++ Rancho_de_gorditos/Assets/Rancho/Scripts/Añadir_Mejorar_Parcela.cs
@@ -4,24 +4,63 @@ public class Añadir_Mejorar_Parcela : MonoBehaviour
 {
     public GameObject parcela, terreno, parcelaGrande, recogidaH, gallinero;
     private GameObject MenuParcela;
+<<<<<<< Updated upstream
     public Transform punto, punto_grande, puntoRecogida, puntoGallinero, puntoGalli2;
     [HideInInspector] public GameObject parcelaPrefab, recogidaPrefab, gallineroPrefab;
     [HideInInspector] public bool hayParcela;
+=======
+    private float dist = 1f;
+    public Transform punto;
+    [HideInInspector] public GameObject parcelaPrefab;
+    [HideInInspector] public bool hayParcela, P_Gallinas=false, P_Vacas = false, P_Cerdos = false, P_Ovejas = false;
+>>>>>>> Stashed changes
     void Start()
     {
         MenuParcela = GameObject.Find("MenuParcelas");
     }
 
-    public void GenerarParcela()
+    public void GenerarParcelaG()
     {
         parcelaPrefab = Instantiate(parcela, punto.position, punto.rotation);
         parcelaPrefab.transform.SetParent(punto);
+<<<<<<< Updated upstream
         parcelaPrefab.transform.localScale = Vector3.one * 0.9f;
         recogidaPrefab =Instantiate(recogidaH, puntoRecogida.position, puntoRecogida.rotation);
         recogidaPrefab.transform.SetParent(puntoRecogida);
         gallineroPrefab = Instantiate(gallinero, puntoGallinero.position, puntoGallinero.rotation);
         gallineroPrefab.transform.SetParent(puntoGallinero);
+=======
+        terreno.tag = "Terreno_Gallinas";
+>>>>>>> Stashed changes
         hayParcela = true;
+        P_Gallinas = true;
+    }
+
+    public void GenerarParcelaV()
+    {
+        parcelaPrefab = Instantiate(parcela, punto.position, punto.rotation);
+        parcelaPrefab.transform.SetParent(punto);
+        terreno.tag = "Terreno_Vacas";
+        hayParcela = true;
+        P_Vacas=true;
+    }
+
+    public void GenerarParcelaC()
+    {
+        parcelaPrefab = Instantiate(parcela, punto.position, punto.rotation);
+        parcelaPrefab.transform.SetParent(punto);
+        terreno.tag = "Terreno_Cerdos";
+        hayParcela = true;
+        P_Cerdos=true;
+    }
+
+    public void GenerarParcelaCa()
+    {
+        parcelaPrefab = Instantiate(parcela, punto.position, punto.rotation);
+        parcelaPrefab.transform.SetParent(punto);
+        terreno.tag = "Terreno_Ovejas";
+        hayParcela = true;
+        P_Ovejas=true;
     }
 
     public void AmpliarParcela()
@@ -47,4 +86,21 @@ public class Añadir_Mejorar_Parcela : MonoBehaviour
         }
     }
 
+<<<<<<< Updated upstream
+=======
+    public void MejoraParcela()
+    {
+        if (P_Gallinas)
+        {
+            GameObject[] gallinas = GameObject.FindGameObjectsWithTag("Gallina");
+        }
+    }
+
+
+
+    void Update()
+    {
+        
+    }
+>>>>>>> Stashed changes
 }
