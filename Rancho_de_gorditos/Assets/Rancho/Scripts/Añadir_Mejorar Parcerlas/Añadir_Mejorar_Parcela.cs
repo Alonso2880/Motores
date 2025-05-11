750228be369849fa00fb5ece4859f1d72052fe9c
Alonso2880 <a80alonso@gmail.com>
2025-05-11 20:26:41 +0200

    cosas

---
+++ Rancho_de_gorditos/Assets/Rancho/Scripts/Añadir_Mejorar Parcerlas/Añadir_Mejorar_Parcela.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class Añadir_Mejorar_Parcela : MonoBehaviour
+{
+    public GameObject parcela, terreno, parcelaGrande, recogidaH, gallinero, recogidaL, recogidaC, recogidaLe;
+    private GameObject MenuParcela;
+    public Transform punto, punto_grande, puntoRecogida, puntoGallinero, puntoGalli2;
+    [HideInInspector] public GameObject parcelaPrefab, recogidaPrefab, gallineroPrefab;
+    [HideInInspector] public bool hayParcela;
+    void Start()
+    {
+        MenuParcela = GameObject.Find("MenuParcelas");
+    }
+
+    public void GenerarParcela()
+    {
+        parcelaPrefab = Instantiate(parcela, punto.position, punto.rotation);
+        parcelaPrefab.transform.SetParent(punto);
+        parcelaPrefab.transform.localScale = Vector3.one * 0.9f;
+        recogidaPrefab =Instantiate(recogidaH, puntoRecogida.position, puntoRecogida.rotation);
+        recogidaPrefab.transform.SetParent(puntoRecogida);
+        gallineroPrefab = Instantiate(gallinero, puntoGallinero.position, puntoGallinero.rotation);
+        gallineroPrefab.transform.SetParent(puntoGallinero);
+        hayParcela = true;
+    }
+
+    public void AmpliarParcela()
+    {
+        Destroy(parcelaPrefab);
+        terreno.transform.localScale = new Vector3(0.9f, 0.9f, 0.9f);
+        parcelaPrefab = Instantiate(parcelaGrande, punto_grande.position, punto_grande.rotation);
+        parcelaPrefab.transform.localScale = Vector3.one * 0.9f;
+        parcelaPrefab.transform.SetParent(punto_grande);
+
+        Destroy(gallineroPrefab);
+        gallineroPrefab = Instantiate(gallinero, puntoGalli2.position, puntoGalli2.rotation);
+        gallineroPrefab.transform.SetParent(puntoGalli2);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Menu_Parcelas m = MenuParcela.GetComponent<Menu_Parcelas>();
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            m.AbrirMenu();
+        }
+    }
+
+}
---
+++ Rancho_de_gorditos/Assets/Rancho/Scripts/Menu_Parcelas/Menu_Parcelas4.cs
@@ -0,0 +1,164 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu_Parcelas4 : MonoBehaviour
+{
+    private GameObject baseParcelas, gallina;
+    public GameObject terreno;
+    private Canvas canvasP;
+    public Button Comprar_Gallinas;
+    public Button Comprar_Vacas;
+    public Button Comprar_Cerdos;
+    public Button Comprar_Ovejas;
+    public Button Mejorar;
+    public Button Ampliar;
+    public Button Salir;
+    [HideInInspector] public bool comprado=false;
+    [HideInInspector] public GameObject contmonedas;
+    void Start()
+    {
+        Comprar_Gallinas.onClick.AddListener(() => ComprarP(1));
+        Comprar_Vacas.onClick.AddListener(() => ComprarP(2));
+        Comprar_Cerdos.onClick.AddListener(() => ComprarP(3));
+        Comprar_Ovejas.onClick.AddListener(() => ComprarP(4));
+        Mejorar.onClick.AddListener(() => Mejoras(1));
+        Ampliar.onClick.AddListener(()=> Mejoras(2));
+        Salir.onClick.AddListener(() => ComprarP(5));
+        Salir.onClick.AddListener(() => Mejoras(3));
+
+        canvasP = GetComponent<Canvas>();
+        canvasP.enabled = false;
+
+        baseParcelas = GameObject.Find("Base de parcelas4");
+        gallina = GameObject.Find("Gallina");
+        contmonedas = GameObject.Find("Canvas");
+
+
+
+
+
+    }
+
+    public void ComprarP(int n)
+    {
+        Añadir_Mejorar_Parcela4 a = baseParcelas.GetComponent<Añadir_Mejorar_Parcela4>();
+        if (comprado == false)
+        {
+            switch (n)
+            {
+                case 1:
+                    a.GenerarParcela();
+                    terreno.tag = "T_Gallinas";
+                    CerrarMenu();
+                    comprado = true;
+                    break;
+
+                case 2:
+                    a.GenerarParcela();
+                    terreno.tag = "T_Vacas";
+                    CerrarMenu();
+                    comprado = true;
+                    break;
+
+                case 3:
+                    a.GenerarParcela();
+                    terreno.tag = "T_Cerdos";
+                    CerrarMenu();
+                    comprado = true;
+                    break;
+
+                case 4:
+                    a.GenerarParcela();
+                    terreno.tag = "T_Ovejas";
+                    CerrarMenu();
+                    comprado = true;
+                    break;
+                case 5:
+                    CerrarMenu();
+                    break;
+            }
+        }
+      
+    }
+
+    public void Mejoras(int n)
+    {
+        Añadir_Mejorar_Parcela4 a = baseParcelas.GetComponent<Añadir_Mejorar_Parcela4>();
+        Gallina g = gallina.GetComponent<Gallina>();
+        Contador_Moneas cont = contmonedas.GetComponent<Contador_Moneas>();
+        if (comprado)
+        {
+            switch (n)
+            {
+                case 1:
+                    if(cont.monedas < 4)
+                    {
+                        Debug.Log("Te faltan monedas");
+                    }
+                    else
+                    {
+                        if (terreno.tag == "T_Gallinas")
+                        {
+                            Gallina.multHuevo = 2;
+                        }
+                        
+                        cont.monedas -= 4;
+                    }
+                    
+                    CerrarMenu();
+                    break;
+                case 2:
+                    if(cont.monedas < 6)
+                    {
+                        Debug.Log("Te faltan monedas");
+                    }
+                    else
+                    {
+                        a.AmpliarParcela();
+                        cont.monedas -= 6;
+                    }
+                    
+                    CerrarMenu();
+                    break;
+                case 3:
+                    CerrarMenu();
+                    break;
+            }
+        }
+
+    }
+
+    public void AbrirMenu()
+    {
+        canvasP.enabled = true;
+        Time.timeScale = 0;
+        if(comprado == false)
+        {
+            Comprar_Gallinas.gameObject.SetActive(true);
+            Comprar_Vacas.gameObject.SetActive(true);
+            Comprar_Cerdos.gameObject.SetActive(true);
+            Comprar_Ovejas.gameObject.SetActive(true);
+            Mejorar.gameObject.SetActive(false);
+            Ampliar.gameObject.SetActive(false);
+
+        }
+        else
+        {
+            Comprar_Gallinas.gameObject.SetActive(false);
+            Comprar_Vacas.gameObject.SetActive(false);
+            Comprar_Cerdos.gameObject.SetActive(false);
+            Comprar_Ovejas.gameObject.SetActive(false);
+            Mejorar.gameObject.SetActive(true);
+            Ampliar.gameObject.SetActive(true);
+
+        }
+    }
+
+    public void CerrarMenu()
+    {
+        canvasP.enabled = false;
+        Time.timeScale = 1;
+    }
+    
+}
